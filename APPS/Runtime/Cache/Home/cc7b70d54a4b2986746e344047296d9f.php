867fb6e9d1acf7e9689083bf118de59feb7182e9
lancelotpro <lancelot.pro@outlook.com>
2017-10-26 12:02:28 +0800

    finished front show half of
     half of it.Do it afternoon!!

--- APPS/Runtime/Cache/Home/cc7b70d54a4b2986746e344047296d9f.php
+++ APPS/Runtime/Cache/Home/cc7b70d54a4b2986746e344047296d9f.php
@@ -41,10 +41,132 @@
 		</div>
 	</div>
 
-	<div>
-		<img src="/TPPHP/Public/images/sep_big.png">
+	<!-- 分割线 -->
+	<div class="sep">
+		<div class="center c_sep">
+			<img src="/TPPHP/Public/images/sep_big.png">
+		</div>
 	</div>
 
+	<!-- 轮播图  -->
+	<div class="carousel">
+		<div class="center c_carousel">
+			<div class="carousel_panel">
+				
+			</div>
+		</div>
+	</div>
+
+	<!-- 欢迎&关注  -->
+	<div class="welcome">
+		<div class="center c_welcome">
+			<div class="w_left fl">
+				<h3>欢迎来到文峰视听</h3>
+				<p>用创造演绎经典，用经典来诠释时尚，用时尚去追求完美，用完美期待永恒，创意，没有结束，只有开始...</p>
+			</div>
+			<div class="w_sep fl">
+				<img src="/TPPHP/Public/images/sep_scoial_icons.png">
+			</div>
+			<div class="w_right fr">
+				<h3>关注我们</h3>
+				<p>点击下方图标</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- 最新动态 -->
+	<div class="sep_ZXDT">
+		<div class="center c_ZXDT">
+			<div class="c_ZXDT_sep">
+				<img src="/TPPHP/Public/images/sep_big.png">
+			</div>
+			<div class="font_warning">
+				<span>最新动态</span>
+			</div>
+			<div class="c_ZXDT_imglist">
+				<div class="ZXDT_img fl">
+					<img src="/TPPHP/Public/images/tms.jpg">
+					<a href="">天猫双十一购物狂欢节筹备</a>
+					<img src="/TPPHP/Public/images/sep_small.png">
+				</div>
+				<div class="ZXDT_img fl">
+					<img src="/TPPHP/Public/images/lssjs.jpg">
+					<a href="">低碳、绿色、节能</a>
+					<img src="/TPPHP/Public/images/sep_small.png">
+				</div>
+				<div class="ZXDT_img fl">
+					<img src="/TPPHP/Public/images/ktzx212.jpg">
+					<a href="">文峰视听在线课堂</a>
+					<img src="/TPPHP/Public/images/sep_small.png">
+				</div>
+				<div class="ZXDT_img fl">
+					<img src="/TPPHP/Public/images/lyqs.jpg">
+					<a href="">连衣裙+小西装 优雅又大方</a>
+					<img src="/TPPHP/Public/images/sep_small.png">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- 时尚生活推荐 -->
+	<div class="sep_SSSH">
+		<div class="center c_SSSH">
+			<div class="c_SSSH_sep">
+				<img src="/TPPHP/Public/images/sep_big.png">
+			</div>
+			<div class="font_warning">
+				<span>时尚生活推荐</span>
+			</div>
+			<div class="c_SSSH_list fl">
+				<a href="">东南亚最值得去的八大海滩</a>
+				<p><img src="/TPPHP/Public/images/sep_small.png"></p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+			</div>
+			<div class="c_SSSH_list fl">
+				<a href="">东南亚最值得去的八大海滩</a>
+				<p><img src="/TPPHP/Public/images/sep_small.png"></p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+			</div>
+			<div class="c_SSSH_list fl">
+				<a href="">东南亚最值得去的八大海滩</a>
+				<p><img src="/TPPHP/Public/images/sep_small.png"></p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+			</div>
+			<div class="c_SSSH_list fl">
+				<a href="">东南亚最值得去的八大海滩</a>
+				<p><img src="/TPPHP/Public/images/sep_small.png"></p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+				<p>马尔代夫双鱼岛()</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- 公告栏  -->
+	<!-- 最新动态 -->
+	<div class="sep_GGL">
+		<div class="center c_GGL">
+			<div class="c_GGL_sep">
+				<img src="/TPPHP/Public/images/sep_medium_home.png">
+			</div>
+			<div class="font_warning">
+				<span>最新动态</span>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- 最新微博信息  -->
 
 </body>
 </html>
